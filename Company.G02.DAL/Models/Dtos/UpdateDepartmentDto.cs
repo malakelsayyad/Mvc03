b94708fe20c6054abbc7de1e1fa60b73e478201b
malakelsayyad <malakelsayyad@yahoo.com>
2025-03-09 22:58:20 +0200

    FInished department

---
+++ Company.G02.DAL/Models/Dtos/UpdateDepartmentDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G02.DAL.Models.Dtos
+{
+    public class UpdateDepartmentDto
+    {
+        [Required(ErrorMessage = "Code is required!")]
+        public string Code { get; set; }
+
+        [Required(ErrorMessage = "Name is required!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Date is required!")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
