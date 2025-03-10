fc7edd7adff9a6f29c5dc6885020199cb11b81eb
malakelsayyad <malakelsayyad@yahoo.com>
2025-03-10 02:23:39 +0200

    Implementation of BLL (Employee)

---
+++ Company.G02.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Company.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G02.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employee> GetAll();
+        Employee? GetById(int id);
+        int Add(Employee department);
+        int Update(Employee department);
+        int Delete(Employee department);
+    }
+}
---
+++ Company.G02.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,46 @@
+﻿using Company.G02.BLL.Interfaces;
+using Company.G02.DAL.Data.Context;
+using Company.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G02.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly CompanyDbContext _context;
+
+        public EmployeeRepository(CompanyDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Employee> GetAll()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public Employee? GetById(int id)
+        {
+            return _context.Employees.Find(id);
+        }
+        public int Add(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            return _context.SaveChanges();
+        }
+
+        public int Update(Employee employee)
+        {
+            _context.Employees.Update(employee);
+            return _context.SaveChanges();
+        }
+        public int Delete(Employee employee)
+        {
+            _context.Employees.Remove(employee);
+            return _context.SaveChanges();
+        }
+    }
+}
