4e95d31e9743b733744d571db61dbb4d1fb7ffa3
malakelsayyad <malakelsayyad@yahoo.com>
2025-04-05 21:40:26 +0200

    fixes

--- Company.G02.DAL/Data/Context/CompanyDbContext.cs
+++ Company.G02.DAL/Data/Context/CompanyDbContext.cs
@@ -20,6 +20,38 @@ namespace Company.G02.DAL.Data.Context
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole()
+            {
+                Id = "C4D17A15-B5A6-41BD-9B6E-B10F86CB48EE",
+                Name="Admin",
+                NormalizedName="ADMIN"
+            });
+            modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole()
+            {
+                Id = "E11E65D8-18C4-493F-AF19-4A3B80F8AC01",
+                Name="User",
+                NormalizedName="USER"
+            });
+
+            PasswordHasher<AppUser> passwordHasher = new PasswordHasher<AppUser>();
+
+
+            modelBuilder.Entity<AppUser>().HasData(new AppUser()
+            {
+                Id = "4EA19E03-C84D-453C-A6B2-CF3A9981F594",
+                FirstName="malak",
+                LastName="elsayyad",
+                Email="malakelsayyad@gmail.com",
+                PasswordHash=passwordHasher.HashPassword(null,"12345Malak@")
+
+            });
+
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>() {
+              
+                RoleId= "C4D17A15-B5A6-41BD-9B6E-B10F86CB48EE",
+              UserId= "4EA19E03-C84D-453C-A6B2-CF3A9981F594"
+
+            });
 
             base.OnModelCreating(modelBuilder);
 
--- Company.G02.DAL/Migrations/20250405190437_initial create.Designer.cs
+++ Company.G02.DAL/Migrations/20250405190437_initial create.Designer.cs
@@ -4,16 +4,19 @@ using Company.G02.DAL.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Company.G02.DAL.Data.Migrations
+namespace Company.G02.DAL.Migrations
 {
     [DbContext(typeof(CompanyDbContext))]
-    partial class CompanyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250405190437_initial create")]
+    partial class initialcreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
